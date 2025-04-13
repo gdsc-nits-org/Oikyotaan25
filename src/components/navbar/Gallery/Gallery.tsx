9199e757e08f6c4c72231d012420bfeefaf834b2
Subrata-20 <subrata_ug_23@ece.nits.ac.in>
2025-04-14 00:44:32 +0530

    Gallery updates

--- src/components/navbar/Gallery/Gallery.tsx
+++ src/components/navbar/Gallery/Gallery.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import type { FC } from "react";
 import { GoTriangleLeft, GoTriangleRight } from "react-icons/go";
+import { useMediaQuery } from "react-responsive";
 
 const images = [
   { id: 1, name: "", image: "/assets/gallery/img6.png" },
@@ -9,6 +10,8 @@ const images = [
   { id: 3, name: "", image: "/assets/gallery/img6.png" },
   { id: 4, name: "", image: "/assets/gallery/img6.png" },
 ];
+
+
 
 const Gallery: React.FC = () => {
   const [current, setCurrent] = useState(0);
@@ -20,17 +23,18 @@ const Gallery: React.FC = () => {
   const handleNext = () => {
     setCurrent((prev) => (prev - 1 + images.length) % images.length);
   };
+  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
     <>
       <style>{`
         @keyframes horizontal1 {
           0%, 100% { transform: translateX(0); }
-          50% { transform: translateX(50px); }
+          50% { transform: ${isTablet ? 'translateX(-20px)' : 'translateX(-50px)'}; }
         }
         @keyframes horizontal2 {
           0%, 100% { transform: translateX(0); }
-          50% { transform: translateX(-50px); }
+          50% { transform: ${isTablet ? 'translateX(-20px)' : 'translateX(-50px)'}; }
         }
         .animate-horizontal1 {
           animation: horizontal1 5s ease-out infinite;
@@ -44,39 +48,39 @@ const Gallery: React.FC = () => {
         className="min-h-screen h-max overflow-hidden bg-[#E3C7A1] flex flex-col items-center justify-center"
         style={{
           backgroundImage: "url('assets/gallery/bg.png')",
-          backgroundSize: "contain",
+          backgroundSize: "cover",
           backgroundRepeat: "repeat-y"
         }}
       >
-        <img src="/assets/gallery/img1.png" alt="" className="absolute h-[50%] top-[11%] left-[2%]" />
+        <img src="/assets/gallery/img1.png" alt="" className="absolute mobile:h-[40vw] laptop:h-[25rem] mobile:top-[13%] laptop:top-[11%] left-[2%]" />
 
-        <div className="h-[69%]">
-          <h1 className="relative font-aprillia text-8xl z-10 translate-y-[220%]">Photo Gallery</h1>
+        <div >
+          <h1 className="relative font-aprillia mx-auto mobile:text-3xl laptop:text-8xl z-10 translate-y-[220%]">Photo Gallery</h1>
           <img
             src="/assets/gallery/img2.png"
             alt=""
-            className="absolute h-[40%] top-[16%] left-[50%] -translate-x-[50%]"
+            className="absolute mobile:h-[32vw] laptop:h-[40%] laptop:top-[16%] left-[50%] -translate-x-[50%]"
           />
         </div>
 
-        <div className="absolute w-[42vh] h-[42vh] top-[13%] right-0">
+        <div className="absolute mobile:w-[32vw] mobile:h-[32vh] laptop:w-[42vh] laptop:h-[42vh] mobile:top-[15%] laptop:top-[13%] right-0">
           <img
             src="/assets/gallery/grp1.png"
             alt="Art Right 1"
             className="absolute top-0 left-[20%] -translate-x-1/2 z-10"
           />
         </div>
+:
+        <div className="mobile:h-[25vh] laptop:h-[55vh]" />
 
-        <div className="h-[55vh]" />
-
-        <div className="relative flex h-[400px] w-screen-md scale-100 items-center justify-center ipadair:scale-50 xl:scale-100 4k:h-[1200px] z-10 mb-10">
+        <div className="relative flex h-[400px] w-screen-md scale-100 items-center justify-center ipadair:scale-50 xl:scale-100 z-10 mb-10">
           {images.map((img, index) => {
             const position = (index - current + images.length) % images.length;
             let styles: React.CSSProperties = {};
 
             if (position === 0) {
               styles = {
-                transform: "scale(1) rotate(-10deg) translateX(-24vw)",
+                transform: `scale(1) ${isTablet ? 'rotate(5deg)': 'rotate(-10deg) translateX(-24vw)'}`,
                 zIndex: 10,
                 opacity: 1,
               };
@@ -97,16 +101,16 @@ const Gallery: React.FC = () => {
             return (
               <div
                 key={img.id}
-                className="absolute transition-all duration-1000 ease-in-out mobile:top-20 4k:top-52"
+                className="absolute transition-all duration-1000 ease-in-out"
                 style={styles}
               >
-                <div className="h-[47vh] w-[64vh] bg-[url('/assets/gallery/img7.png')] bg-contain bg-center px-11 py-9.5">
+                <div className="mobile:h-[50vw] mobile:w-[70vw] laptop:h-[47vh] laptop:w-[64vh] bg-[url('/assets/gallery/img7.png')] bg-contain bg-center mobile:px-7 laptop:px-11.5 items-center justify-center flex">
                   <img src={img.image} alt="" />
                 </div>
                 <img
                   src="/assets/gallery/Group.png"
                   alt=""
-                  className="relative h-[12rem] mx-auto -translate-y-[3.2rem] -z-10"
+                  className="relative mobile:h-[6.5rem] laptop:h-[12rem] mx-auto mobile:-translate-y-[1.5rem] laptop:-translate-y-[3.2rem] -z-10"
                 />
               </div>
             );
@@ -130,17 +134,18 @@ const Gallery: React.FC = () => {
           </button>
         </div>
 
-        <div className="-translate-y-[7rem] relative z-10">
-          <img src="/assets/gallery/grp2.png" alt="" />
+        <div className="mobile:-translate-y-[13rem] laptop:-translate-y-[7rem] relative z-10">
+          <img src="/assets/gallery/grp2.png" alt="" 
+          className="scale-125 laptop:scale-100"/>
           <img
             src="/assets/gallery/grp3.png"
             alt=""
-            className="absolute top-[2rem] -left-[16rem] w-full z-30 animate-horizontal2"
+            className="absolute top-0 laptop:top-[2rem] mobile:scale-125 laptop:scale-100 mobile:-left-[4rem] laptop:-left-[16rem] w-full z-30 animate-horizontal2"
           />
           <img
             src="/assets/gallery/grp4.png"
             alt=""
-            className="absolute top-[4rem] left-[4.5rem] w-[150%] scale-120 z-30 animate-horizontal1"
+            className="absolute top-0 laptop:top-[4rem] laptop:left-[4.5rem] w-[150%] mobile:scale-140 laptop:scale-120 z-30 animate-horizontal1"
           />
         </div>
       </div>
