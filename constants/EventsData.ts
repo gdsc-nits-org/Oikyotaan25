9c12e356020d5c90356c0d7c969af0f0ca6c4e36
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2025-04-14 00:05:07 +0530

    Added Events Page

---
+++ constants/EventsData.ts
@@ -0,0 +1,27 @@
+export type EventList = {
+    title : string;
+    desc : string;
+};
+
+export const EventData: EventList[] = [
+    {
+        title: "Morning Rally",
+        
+        desc: "Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac ."
+    },
+    {
+        title: "Afternoon Rally",
+
+        desc: "Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac ."
+    },
+    {
+        title: "Night Rally",
+
+        desc: "Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac ."
+    },
+    {
+        title: "Evening Rally",
+
+        desc: "Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.Vorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac ."
+    },
+]
---
+++ public/EventsPhotos/EventsPhotos.ts
@@ -0,0 +1,27 @@
+export const EventPhotos = {
+    bgImage : "https://res.cloudinary.com/dludtk5vz/image/upload/v1744127816/WhatsApp_Image_2025-04-08_at_19.37.54_f3330aa5_noek5y.jpg",
+
+    cardDeco1 : "https://res.cloudinary.com/dludtk5vz/image/upload/v1744270557/WhatsApp_Image_2025-04-10_at_11.12.10_a899932c_zmsk66.png",
+
+    cardDeco2: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744270644/WhatsApp_Image_2025-04-10_at_11.12.30_97c6a8dc_vuglqz.png",
+
+    cardDeco3: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744271641/WhatsApp_Image_2025-04-10_at_13.21.50_cdd5de56_bsvdjs.png",
+
+    cardDeco4: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744271729/WhatsApp_Image_2025-04-10_at_13.22.06_aa2aecef_feyewt.png",
+
+    cardImg: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744275496/b230f9ed852c5822de93abde64c84602_gzlcab.jpg",
+
+    navButton: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744538899/WhatsApp_Image_2025-04-13_at_15.36.55_e7480327-removebg-preview_cg1kqh.png",
+
+    ball1: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744563959/WhatsApp_Image_2025-04-13_at_21.32.47_faa0d315-removebg-preview_ucsmna.png",
+
+    ball2: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744564193/WhatsApp_Image_2025-04-13_at_22.39.18_82d1191a-removebg-preview_fitl58.png",
+
+    ball3: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744564435/WhatsApp_Image_2025-04-13_at_22.42.59_4c4286e0-removebg-preview_tvi89t.png",
+
+    ball4: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744564651/WhatsApp_Image_2025-04-13_at_22.44.11_cfe311c5-removebg-preview_cupugn.png",
+
+    ball5: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744564671/WhatsApp_Image_2025-04-13_at_22.46.52_55324a54-removebg-preview_rghpno.png",
+
+    ball6: "https://res.cloudinary.com/dludtk5vz/image/upload/v1744564813/WhatsApp_Image_2025-04-13_at_22.49.33_115736f4-removebg-preview_ckbarw.png",
+}
