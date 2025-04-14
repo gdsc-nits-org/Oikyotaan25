8c2db734a35b54fc57a5c4d7a241503be8375f63
Subrata-20 <subrata_ug_23@ece.nits.ac.in>
2025-04-14 23:15:45 +0530

    Gallery updates

--- src/components/navbar/Gallery/Gallery.tsx
+++ src/components/navbar/Gallery/Gallery.tsx
@@ -52,10 +52,10 @@ const Gallery: React.FC = () => {
           backgroundRepeat: "repeat-y"
         }}
       >
-        <img src="/assets/gallery/img1.png" alt="" className="absolute mobile:h-[40vw] laptop:h-[25rem] mobile:top-[13%] laptop:top-[11%] left-[2%]" />
+        <img src="/assets/gallery/img1.png" alt="" className="absolute mobile:h-[40vw] laptop:h-[25rem] mobile:top-[13%] tablet:top-[18%] laptop:top-[11%] left-[2%]" />
 
         <div >
-          <h1 className="relative font-aprillia mx-auto mobile:text-3xl laptop:text-8xl z-10 translate-y-[220%]">Photo Gallery</h1>
+          <h1 className="relative font-aprillia mx-auto mobile:text-3xl tablet:text-6xl laptop:text-8xl z-20 mobile:translate-y-[220%] tablet:translate-y-[240%] ipad:translate-y-[300%] laptop:translate-y-[220%]">Photo Gallery</h1>
           <img
             src="/assets/gallery/img2.png"
             alt=""
@@ -63,7 +63,7 @@ const Gallery: React.FC = () => {
           />
         </div>
 
-        <div className="absolute mobile:w-[32vw] mobile:h-[32vh] laptop:w-[42vh] laptop:h-[42vh] mobile:top-[15%] laptop:top-[13%] right-0">
+        <div className="absolute mobile:w-[32vw] mobile:h-[32vh] laptop:w-[42vh] laptop:h-[42vh] mobile:top-[15%] tablet:top-[21%] laptop:top-[13%] right-0">
           <img
             src="/assets/gallery/grp1.png"
             alt="Art Right 1"
@@ -71,7 +71,7 @@ const Gallery: React.FC = () => {
           />
         </div>
 :
-        <div className="mobile:h-[25vh] laptop:h-[55vh]" />
+        <div className="mobile:h-[25vh] tablet:h-[40vh] laptop:h-[55vh] ipad:h-[45vh]" />
 
         <div className="relative flex h-[400px] w-screen-md scale-100 items-center justify-center ipadair:scale-50 xl:scale-100 z-10 mb-10">
           {images.map((img, index) => {
@@ -104,13 +104,13 @@ const Gallery: React.FC = () => {
                 className="absolute transition-all duration-1000 ease-in-out"
                 style={styles}
               >
-                <div className="mobile:h-[50vw] mobile:w-[70vw] laptop:h-[47vh] laptop:w-[64vh] bg-[url('/assets/gallery/img7.png')] bg-contain bg-center mobile:px-7 laptop:px-11.5 items-center justify-center flex">
+                <div className="mobile:h-[50vw] mobile:w-[70vw] tablet:h-[37vw] tablet:w-[50vw] laptop:h-[47vh] laptop:w-[64vh] bg-[url('/assets/gallery/img7.png')] bg-contain bg-center mobile:px-7 tablet:px-9 laptop:px-11.5 items-center justify-center flex">
                   <img src={img.image} alt="" />
                 </div>
                 <img
                   src="/assets/gallery/Group.png"
                   alt=""
-                  className="relative mobile:h-[6.5rem] laptop:h-[12rem] mx-auto mobile:-translate-y-[1.5rem] laptop:-translate-y-[3.2rem] -z-10"
+                  className="relative mobile:h-[6.5rem] tablet:h-[10rem] ipad:h-[12rem] mx-auto mobile:-translate-y-[1.5rem] tablet:-translate-y-[3.2rem] -z-10"
                 />
               </div>
             );
@@ -134,13 +134,13 @@ const Gallery: React.FC = () => {
           </button>
         </div>
 
-        <div className="mobile:-translate-y-[13rem] laptop:-translate-y-[7rem] relative z-10">
+        <div className="mobile:-translate-y-[13rem] tablet:-translate-y-[10rem] laptop:-translate-y-[7rem] relative z-10">
           <img src="/assets/gallery/grp2.png" alt="" 
           className="scale-125 laptop:scale-100"/>
           <img
             src="/assets/gallery/grp3.png"
             alt=""
-            className="absolute top-0 laptop:top-[2rem] mobile:scale-125 laptop:scale-100 mobile:-left-[4rem] laptop:-left-[16rem] w-full z-30 animate-horizontal2"
+            className="absolute top-0 laptop:top-[2rem] mobile:scale-125 laptop:scale-100 mobile:-left-[4rem] tablet:-left-[8rem] laptop:-left-[16rem] w-full z-30 animate-horizontal2"
           />
           <img
             src="/assets/gallery/grp4.png"
